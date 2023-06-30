41a80dff358750d147b3d376742528d56af9adb3
Smokost <smokost@gmail.com>
2023-06-30 17:10:01 +0200

    Init config

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='asyncio_functools',
+    version='0.0.1',
+    author='Appuxif',
+    author_email='app@mail.com',
+    description='A Python package with functools working with asyncio coroutines',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Appuxif/asyncio_functools',
+    project_urls={
+        'Bug Tracker': 'https://github.com/Appuxif/asyncio_functools/issues',
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    package_dir={'': '.'},
+    packages=['asyncio_functools'],
+    package_data={},
+    python_requires='>=3.8',
+)
